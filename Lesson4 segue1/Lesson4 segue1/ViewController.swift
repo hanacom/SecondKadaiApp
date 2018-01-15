369e62d202af2505adbed1a93856644ed70646d7
増田華子 <masudahanako@masudakazokunoimac.local>
2018-01-15 21:53:04 +0900

    画面変遷ができるようにした。。。はず

---
+++ Lesson4 segue1/Lesson4 segue1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lesson4 segue1
+//
+//  Created by 増田華子 on 2018/01/14.
+//  Copyright © 2018年 hanako.masuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Lesson4 segue2/Lesson4 segue2/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Lesson4 segue2
+//
+//  Created by 増田華子 on 2018/01/14.
+//  Copyright © 2018年 hanako.masuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
