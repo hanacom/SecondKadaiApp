03747d0be8b03e4beee928ef01bd71bd122b5196
増田華子 <masudahanako@masudakazokunoimac.local>
2018-01-20 14:46:49 +0900

    画面遷移ができるようにした。 まだ元の画面には戻せない。 未実行。エラーなし。

--- SecondKadaiApp/SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/SecondKadaiApp/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        
+        
+    }
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
