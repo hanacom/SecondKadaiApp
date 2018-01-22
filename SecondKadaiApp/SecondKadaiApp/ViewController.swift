2fa91dffdc0b093e9a44fd9f6be212bbe024e6a1
増田華子 <masudahanako@masudakazokunoimac.local>
2018-01-22 19:27:14 +0900

    完成！ キーボードが閉じない...

--- SecondKadaiApp/SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/SecondKadaiApp/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     
     
@@ -24,16 +24,16 @@ class ViewController: UIViewController {
     
     
     
-   
-    
-   
-    
-    override func viewDidLoad() {
+   override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+    
+    
+    
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
